e09545395fdaf690229ca109f1efa1892901a3be
Lim-Huiee <76673664+Lim-Huiee@users.noreply.github.com>
2021-12-18 14:11:45 +0800

    productCard and update navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react'
 
 import NavBar from "./components/NavBar";
+import ProductDisplay from './components/ProductDisplay';
 
 function App() {
   return (
@@ -22,7 +23,10 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <NavBar /> 
+    <div>
+      <NavBar /> 
+      <ProductDisplay />
+    </div>
 
   );
 }
